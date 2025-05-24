cf09b893bfe0f86857c91c8e3a7fde5957d80b9f
Hyun149 <rusia4527@Gmail.com>
2025-05-24 23:17:03 +0900

    feat: 점프 기능 및 Ground 체크 로직 추가
    - PlayerMovement.cs에 점프 기능 구현
      - Rigidbody.AddForce 방식 사용
      - groundLayer 기반 CheckSphere로 착지 상태 판단
    - StartScene에서 groundCheck 오브젝트 및 관련 설정 추가

--- Basic_3D_RPG/Assets/Scripts/Players/PlayerMovement.cs
+++ Basic_3D_RPG/Assets/Scripts/Players/PlayerMovement.cs
@@ -11,11 +11,17 @@ public class PlayerMovement : MonoBehaviour, IPlayerActions
     [Header("Look")]
     [SerializeField] private PlayerLook lookHandler;
 
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.3f;
 
     private Rigidbody rb;
     private PlayerInputHandler inputHandler;
     private Vector2 currentInput;
+
+    private bool isGrounded;
 
     private void Awake()
     {
@@ -27,6 +33,7 @@ public class PlayerMovement : MonoBehaviour, IPlayerActions
 
     private void FixedUpdate()
     {
+        CheckGround();
         Vector3 move = new Vector3(currentInput.x, 0f, currentInput.y);
         Vector3 worldMove = transform.TransformDirection(move);
         rb.MovePosition(rb.position + worldMove * moveSpeed * Time.fixedDeltaTime);
@@ -39,11 +46,19 @@ public class PlayerMovement : MonoBehaviour, IPlayerActions
 
     public void OnJump()
     {
-
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 
     public void OnLook(Vector2 lookInput)
     {
         lookHandler?.ApplyLook(lookInput);
+    }
+
+    private void CheckGround()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
     }
 }
